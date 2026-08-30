91640015bb441cbbba6a930d7e62102d2020e227
Teaching2504 <tuyettrinhnguyenthi25042005@gmail.com>
2026-08-31 00:13:29 +0700

    test: add application tests

---
+++ SpringStudentSuccessHubApp/src/test/java/com/nttt/springstudentsuccesshubapp/SpringStudentSuccessHubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttt.springstudentsuccesshubapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentSuccessHubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
